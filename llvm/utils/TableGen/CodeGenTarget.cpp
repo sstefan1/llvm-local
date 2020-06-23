28a4c40565c13ff10c656f2a94d7ffaba14dd34c
sstefan1 <sstipanovic@s-energize.com>
2020-06-23 22:44:32 +0200

    [IR] Intrinsics attribute opt-out list proposal.
    Summary: This patch introduces an opt-out attribute list for intrinsics, discussed in D65377.
    This should make it easier to annotate intrinsics with attributes that apply to most of them.
    Right now I added NoFree and NoSync. If this looks ok, I'll also replace "Throws" IntrinsicProperty with IntrNoUnwind
    and make it default.

--- llvm/utils/TableGen/CodeGenTarget.cpp
+++ llvm/utils/TableGen/CodeGenTarget.cpp
@@ -619,6 +619,7 @@ CodeGenIntrinsic::CodeGenIntrinsic(Record *R) {
   canThrow = false;
   isNoReturn = false;
   isNoSync = false;
+  isNoFree = false;
   isWillReturn = false;
   isCold = false;
   isNoDuplicate = false;
@@ -785,6 +786,8 @@ CodeGenIntrinsic::CodeGenIntrinsic(Record *R) {
       isNoReturn = true;
     else if (Property->getName() == "IntrNoSync")
       isNoSync = true;
+    else if (Property->getName() == "IntrNoFree")
+      isNoFree = true;
     else if (Property->getName() == "IntrWillReturn")
       isWillReturn = true;
     else if (Property->getName() == "IntrCold")
